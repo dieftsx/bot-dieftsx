285461f18f270c5936932a78f000062886c1bcfb
dieftsx <diefersonfrontenddeveloper@gmail.com>
2026-05-20 22:41:50 -0400

    Feat: Finalized endpoints in main.go

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+"bot-dieftsx"
 
 
+func main() {
+	//Mapeamento dos endpoints chamando as funções do pacote Handlers
+	http.HandleFunc("/api/data", handlers.DataHandler)
+	http.HadlerFunc("/api/update", handlers.UpdateStatusHandlers)
+	http.HandlerFunc("/api/update_project", handlers.UpdateProjectHandler)
+	http.HandlerFunc("/api/end_live", handlers.EndLiveHandler)
+
+	//Servidor de Arquivos Estáticos (Apontando para a pasta Frontend)
+	fs := http.FileServer(http.Dir("../frontend/))
+	http.Handle("/", fs)
+
+	log.PrintLn("⚡ Bot rodando em http://localhost:8080")
+	log.PrintLn("📡 Servidor frontend da pasta: ../frontend/")
+	log.Fatal(http.ListenAndServer(":8080", nil))
+
+}
 
 
 
