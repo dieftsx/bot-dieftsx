aecc159521328bad621127c47f4eaeb3e0cdb51d
dieftsx <diefersonfrontenddeveloper@gmail.com>
2026-05-20 21:29:39 -0400

    Chore: Finalized Data Structure

--- backend/handlers/api.go
+++ backend/handlers/api.go
@@ -7,6 +7,26 @@ import (
 
 
 // Estrutura de Dados
+type ProjectInfo struct {
+				Directory string `json:"directory"`
+				Branch string `json:"branch"`
+				Status string `json:"status"`
+}
+
+type LiveStats struct {
+				Duration string `json:"duration"`
+				Lines string `json:"lines`
+				Commits string `json:"commits"`
+				Messages string `json:messages"`
+}
+type StreamData struct {
+		  	Status 			string 					`json:"status"`
+				SystemInfo map[string]string `json:"system_info"`
+				TechStack []string           `json:"tech_stack"`
+				Project ProjectInfo          `json:"project"`
+				Stats   LiveStats							`json:"stats"`
+
+}
 // Estado Inicial (Vamos notar o 'Letra Maiúscula' para ser exportado, caso o main precise ler diretamente)
 // DataHandler para retornar todos os dados para o Frontend
 // UpdateStatusHandler para atualizar o Status(ex: Café, Codando)
