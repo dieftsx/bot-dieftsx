bbe4a9f91d91fe2edee0ebb357a66e7b6550ce6e
dieftsx <diefersonfrontenddeveloper@gmail.com>
2026-05-20 20:07:00 -0400

    Feat: Config And Structure project

---
+++ backend/handlers/api.go
@@ -0,0 +1,14 @@
+package handlers
+
+import (
+	"enconding/json"
+	"net/http"
+)
+
+
+// Estrutura de Dados
+// Estado Inicial (Vamos notar o 'Letra Maiúscula' para ser exportado, caso o main precise ler diretamente)
+// DataHandler para retornar todos os dados para o Frontend
+// UpdateStatusHandler para atualizar o Status(ex: Café, Codando)
+// UpdateProjectHandler atualiza o projeto atual (via terminal)
+// EndLiveHandler registra os status finais da live
